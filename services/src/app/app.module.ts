b46755463d162130d0cc589bd4b31bfd2a3f8120
Harshitha <harshitharathod275@gmail.com>
2020-04-01 18:54:54 +0200

    New version

--- services/src/app/app.module.ts
+++ services/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { EmployeelistComponent } from './employeelist/employeelist.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
